a181dd2b55b02080a4ad131ec2a84a40fe9825c8
RAG-home <rgilmore@umc.edu>
2018-06-05 23:47:43 -0500

    Removed post_stup script which is for the end user to add custom functionality.

--- renv/core.py
+++ renv/core.py
@@ -216,15 +216,6 @@ class RenvBuilder(EnvBuilder):
     def install_scripts(self, context, path):
         pass
 
-    def post_setup(self, context):
-        """
-        Hook for post-setup modification of the venv. Subclasses may install
-        additional packages or scripts here, add activation shell scripts, etc.
-        :param context: The information for the environment creation request
-                        being processed.
-        """
-        pass
-
     def setup_r_profile(self):
         # New
         pass
