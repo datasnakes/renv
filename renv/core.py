d0f6ffbbd4ed5c14ac6b72877d154e4e3a2bfa87
RAG-home <rgilmore@umc.edu>
2018-06-05 22:03:05 -0500

    Finished model for getting context for R environment.

--- renv/core.py
+++ renv/core.py
@@ -52,6 +52,10 @@ class RenvBuilder(EnvBuilder):
         prompt = self.prompt if self.prompt is not None else context.env_name
         context.prompt = '(%s) ' % prompt
         create_if_needed(env_dir)
+        r_exe = "FAKE EXECUTABLE//PATH//R.exe"
+        context.R_abs_exe = r_exe
+        r_script = "FAKE EXECUTABLE//PATH//Rscript"
+        context.R_abs_script = r_script
         # TODO-ROB: Create a function for finding the R executable
         # TODO-ROB:  This may be tied in with a config file or with an outside environment variable.
         # env = os.environ
@@ -59,7 +63,45 @@ class RenvBuilder(EnvBuilder):
         #     executable = os.environ['__PYVENV_LAUNCHER__']
         # else:
         #     executable = sys.executable
+        r_exe_dir, r_exe = os.path.split(os.path.abspath(r_exe))
+        r_script_dir, r_script = os.path.split(os.path.abspath(r_script))
+        context.R_exe_dir = r_exe_dir
+        context.R_script_dir = r_script_dir
+        context.R_exe = r_exe
+        context.R_script = r_script
 
+        # TODO-ROB: Get the package directory information from R
+        # TODO-ROB: This can be relative, because the script just needs the naming convention per os
+        binname = "FAKE_bin"
+        incpath = "FAKE_include"
+        libpath = os.path.join(env_dir, 'FAKE_R', "FAKE_platform", "FAKE_R_X.X.X")
+        # if sys.platform == 'win32':
+        #     binname = 'Scripts'
+        #     incpath = 'Include'
+        #     libpath = os.path.join(env_dir, 'Lib', 'site-packages')
+        # else:
+        #     binname = 'bin'
+        #     incpath = 'include'
+        #     libpath = os.path.join(env_dir, 'lib',
+        #                            'python%d.%d' % sys.version_info[:2],
+        #                            'site-packages')
+
+        context.inc_path = path = os.path.join(env_dir, incpath)
+        create_if_needed(path)
+        create_if_needed(libpath)
+        # TODO-ROB: Do we need this somehow?
+        # Issue 21197: create lib64 as a symlink to lib on 64-bit non-OS X POSIX
+        # if ((sys.maxsize > 2**32) and (os.name == 'posix') and
+        #     (sys.platform != 'darwin')):
+        #     link_path = os.path.join(env_dir, 'lib64')
+        #     if not os.path.exists(link_path):   # Issue #21643
+        #         os.symlink('lib', link_path)
+        context.bin_path = binpath = os.path.join(env_dir, binname)
+        context.bin_name = binname
+        context.env_R_exe = os.path.join(binpath, r_exe)
+        context.env_R_script = os.path.join(binpath, r_script)
+        create_if_needed(binpath)
+        return context
 
     def create_configuration(self, context):
         pass
