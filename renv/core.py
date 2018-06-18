7544f43a6e9cda89541fc9dda1e704d463f1e944
RAG-home <rgilmore@umc.edu>
2018-06-18 01:44:18 -0500

    Added install_scripts because it was using the wrong directory.

--- renv/core.py
+++ renv/core.py
@@ -310,8 +310,51 @@ class RenvBuilder(EnvBuilder):
     # TODO-ROB: Test if the scripts work properly with this build.
     # TODO-ROB: Different variables will need to be created in the replace_variables function.
     # TODO-ROB: The variables will have to be changed in the /scripts/* files.
-    # def install_scripts(self, context, path):
-    #     pass
+    def install_scripts(self, context, path):
+        """
+        Install scripts into the created environment from a directory.
+        :param context: The information for the environment creation request
+                        being processed.
+        :param path:    Absolute pathname of a directory containing script.
+                        Scripts in the 'common' subdirectory of this directory,
+                        and those in the directory named for the platform
+                        being run on, are installed in the created environment.
+                        Placeholder variables are replaced with environment-
+                        specific values.
+        """
+        env_dir = context.env_dir
+        plen = len(path)
+        for root, dirs, files in os.walk(path):
+            if root == path:  # at top-level, remove irrelevant dirs
+                for d in dirs[:]:
+                    if d not in ('common', os.name):
+                        dirs.remove(d)
+                continue  # ignore files in top level
+            for f in files:
+                srcfile = os.path.join(root, f)
+                suffix = root[plen:].split(os.sep)[2:]
+                if not suffix:
+                    dstdir = env_dir
+                else:
+                    dstdir = os.path.join(env_dir, *suffix)
+                if not os.path.exists(dstdir):
+                    os.makedirs(dstdir)
+                dstfile = os.path.join(dstdir, f)
+                with open(srcfile, 'rb') as f:
+                    data = f.read()
+                if not srcfile.endswith('.exe'):
+                    try:
+                        data = data.decode('utf-8')
+                        data = self.replace_variables(data, context)
+                        data = data.encode('utf-8')
+                    except UnicodeError as e:
+                        data = None
+                        logger.warning('unable to copy script %r, '
+                                       'may be binary: %s', srcfile, e)
+                if data is not None:
+                    with open(dstfile, 'wb') as f:
+                        f.write(data)
+                    shutil.copymode(srcfile, dstfile)
 
     def setup_scripts(self, context):
         """
