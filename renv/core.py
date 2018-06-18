221c29345e352d84d72b7953a34b42b6d9f84ba2
RAG-home <rgilmore@umc.edu>
2018-06-18 01:34:33 -0500

    Added setup_scripts because it uses a path relative to venv.

--- renv/core.py
+++ renv/core.py
@@ -313,6 +313,20 @@ class RenvBuilder(EnvBuilder):
     # def install_scripts(self, context, path):
     #     pass
 
+    def setup_scripts(self, context):
+        """
+        Set up scripts into the created environment from a directory.
+        This method installs the default scripts into the environment
+        being created. You can prevent the default installation by overriding
+        this method if you really need to, or if you need to specify
+        a different location for the scripts to install. By default, the
+        'scripts' directory in the venv package is used as the source of
+        scripts to install.
+        """
+        path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(path, 'scripts')
+        self.install_scripts(context, path)
+
     # def install_r(self):
     #     # New: install specified version of R in the R environment.
     #     pass
