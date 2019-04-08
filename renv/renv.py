e3a8fa6b2e1be2056bc51757f2b420c040f2fb99
Rob Gilmore <robgilmore127@gmail.com>
2019-04-08 17:36:26 -0500

    Updated renv parameters and imports.

--- renv/renv.py
+++ renv/renv.py
@@ -1,7 +1,5 @@
 import click
 from renv import BaseRenvBuilder, get_system_venv
-import os
-from pkg_resources import get_distribution
 
 
 @click.group()
@@ -10,8 +8,6 @@ from pkg_resources import get_distribution
                    "installation directory when using ./configure --prefix=<r_home>.")
 @click.option('--env_name', '-n', default=None,
               help="Name of the environment.")
-@click.option('--env_home', '-d', default=None,
-              help="A directory for creating the environment in.")
 @click.option("--path", "-p", default="~/.beRi",
               help="An absolute installation path for renv.", show_default=True)
 @click.option("--name", "-n", default=".renv",
@@ -22,9 +18,6 @@ from pkg_resources import get_distribution
               help="Provide the lib directory if R was installed when using ./configure --libdir=<libpath>.")
 @click.option('--includedir', '-i',
               help="Provide the include directory if R was installed when using ./configure --includedir=<includepath>.")
-@click.option('--system_site_packages', '-sp', type=bool, default=False,
-              help="This determines whether or not the R_LIBS_USER environment variable utilizes the "
-                   "original R's package library as a secondary source for loading packages.")
 @click.option('--recommended_packages', '-rp', type=bool, default=True,
               help="This determines wheather or not the recommended packages are installed in the"
                    "R environment along with the base packages.  In most cases it's best to keep the"
@@ -38,11 +31,9 @@ from pkg_resources import get_distribution
               help="Provide an alternative prompt prefix for this environment.")
 @click.option('--verbose', '-v', is_flag=True,
               help="Show verbose cli output.")
-@click.option('--version', '-V', is_flag=True,
-              help="Show the version of renv and exit.")
 @click.pass_context
-def renv(ctx, r_home, env_name, env_home, path, name, bindir, libdir, includedir, system_site_packages, recommended_packages, clear,
-         upgrade, prompt, verbose, version):
+def renv(ctx, r_home, env_name, path, name, bindir, libdir, includedir, recommended_packages, clear,
+         upgrade, prompt, verbose):
     ctx.ensure_object(dict)
     ctx.obj['path'] = path
     ctx.obj['name'] = name
