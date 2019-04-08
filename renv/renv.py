269269aea42ee6b3614e7eb30bcb0e9d42ca41f5
Rob Gilmore <robgilmore127@gmail.com>
2019-04-08 18:28:41 -0500

    Updated renv cli bug#1

--- renv/renv.py
+++ renv/renv.py
@@ -44,7 +44,7 @@ def renv(ctx, r_home, env_name, path, name, bindir, libdir, includedir, recommen
     ctx.obj['venvR'] = venvR
     venvR(env_name=env_name, path=path, name=name, r_home=r_home, recommended_packages=recommended_packages,
           clear=clear, upgrade=upgrade, prompt=prompt, verbose=verbose, bindir=bindir, libdir=libdir,
-          includedir=includedir)
+          rincludedir=includedir)
 
 
 @renv.command(help="Initialize renv using the <path>/<name>.")
